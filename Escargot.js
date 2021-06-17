503a6fbfb26299b1d9d6072f2c48ce050eb3aaa0
Chloe MORENO <corentin.prevost@wildcodeschool.com>
2021-06-17 12:23:36 +0200

    added pseudocode for snail exercise

---
+++ Escargot.js
@@ -0,0 +1,10 @@
+escargot 1 = 1m par jour il en a 10 à faire //10
+escargot 2 = 1m par jour il en a 5 à faire  //5
+escargot 3 =  1m par jour il en a 8 à faire  //8
+escargot 4 =  1m par jour il en a 3 à faire //3
+escargot 5 =  4m par jour il en a 25 à faire//6.25
+escargot 7 = 2m par jour il en 9 à faire // 4,5
+
+sommet atteint = nombre de mètres à faire / nombre de m par jour
+si un escargot atteint le sommet en 1j (sommet atteint <=1) classer les autres escargot par le nombre de m par jour && par ordre de leur index
+
