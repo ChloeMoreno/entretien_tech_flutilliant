b0e76638b4242bfea38641aa194c468391fd15d0
Chloe MORENO <corentin.prevost@wildcodeschool.com>
2021-06-17 10:19:09 +0200

    did smaller prime exercise, created palindrome exercise file

---
+++ SmallerPrime.js
@@ -0,0 +1,20 @@
+const smallerPrime = (limit) => {
+  let prime = []
+  for (let i = 2; i <= limit; i++){
+    let number = 1;
+    let racine = Math.floor(Math.sqrt(i));
+    do{
+      number++
+    }while(number <= racine && i%number != 0);
+      if(number > racine){
+      prime.push(i)
+      }
+    }
+  return prime
+ };
+
+console.log(smallerPrime(10));
+
+
+
+
